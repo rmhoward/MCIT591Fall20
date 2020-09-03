4c4a90455c1e2388145acae4528c88f20dfef317
rmhoward <rmhoward@seas.upenn.edu>
2020-09-03 15:11:16 +0500

    Finished file
    Signed-off-by: rmhoward <rmhoward@seas.upenn.edu>

--- Lunar Lander.py
+++ Lunar Lander.py
@@ -1,20 +1,20 @@
 
 """
-You are in a lunar module, some distance above the Moon's surface. Gravity is pulling you toward the Moon at an
-ever-increasing rate of speed. You have a limited amount of fuel to use, and each time you burn fuel,
-you reduce your speed by a certain amount. If you burn the right amount of fuel at the right time, you can land
-safely (that is, when you reach the Moon's surface, you are not going too fast).
+Rachael Howard
+82772985
+I worked alone without help.
+Win: 0,0,0,0,10,10,0,10,10,10,10,20,20,0,0
+Loss: 0,0,0,0,0,0,0,0,0,0,0
 """
 
 #Define the function game(), which contains the entire game.
 def game():
 
     #Define initial values for altitude (m), velocity (m/s), fuel remaining (liters), and fuel usage (liters/m/s)
-    altitude = 7000
-    velocity = 1000
-    fuel_remaining = 500
-    fuel_burn = 30
-    burn_constant = 2.5
+    altitude = 100
+    velocity = 0
+    fuel_remaining = 100
+    burn_constant = 0.15
 
 
     #Starting art.
@@ -67,8 +67,8 @@ def game():
 
     #The following data/input loop will run until the altitude is 0 or negative
     while altitude > 0:
-        print("Your current altitude is", altitude)
-        print("Your current velocity is", velocity)
+        print("Your current altitude is", altitude,"m")
+        print("Your current velocity is", round(velocity,2),"m/s")
         print("You currently have",fuel_remaining,"liters of fuel remaining.")
         print()
 
@@ -76,30 +76,36 @@ def game():
         try:
             #Get input from player about how much fuel they want to burn.
             print("How much fuel would you like to burn?")
-            fuel_burn = int(input())
+            fuel_used = int(input())
 
             #Increase maneuver count.
             count += 1
 
-            #If the amount the player inputs is larger than the amount of fuel remaining, set the amount of fuel burned equal to what is remaining in the tank.
-            if fuel_burn > fuel_remaining:
-                fuel_burn = fuel_remaining
-                print("-----------------------------------------")
-                fuel_remaining -= fuel_burn
-
             #If the player enters a negative number, they burn 0 fuel
-            elif fuel_burn < 0:
-                fuel_burn = 0
+            if fuel_used < 0:
+                fuel_used = 0
                 print("-----------------------------------------")
-                fuel_remaining -= fuel_burn
+                fuel_remaining -= fuel_used
 
-            #Velocity and altitude calculations.
-            velocity = velocity + 1.6 - (burn_constant*fuel_burn)
+            #If the amount the player inputs is larger than the amount of fuel remaining, set the amount of fuel burned equal to what is remaining in the tank.
+            elif fuel_used > fuel_remaining:
+                fuel_used = fuel_remaining
+                print("-----------------------------------------")
+                fuel_remaining = 0
+
+            elif fuel_used > 0 and fuel_used <= fuel_remaining:
+                print("-----------------------------------------")
+                fuel_remaining = fuel_remaining - fuel_used
+
+
+            #Velocity increases by 1.6 m/s and decreases by the amount proportional (burn constant) to the fuel used
+            velocity = velocity + 1.6 - (burn_constant*fuel_used)
+            #Altitude decreases by velocity
             altitude -= velocity
 
         except:
             print("-----------------------------------------------------------")
-            print("Invalid number. Please enter a number betwee 0 and", fuel_remaining)
+            print("Invalid number. Please enter a number between 0 and", fuel_remaining)
             print("-----------------------------------------------------------")
             continue
 
@@ -160,15 +166,15 @@ def game():
         #Player wins if velocity is less than 10 m/s
         if velocity < 10:
             print(landersuccessart)
-            print("You've landed safely in",count,"seconds at a velocity of",velocity,".")
+            print("You've landed safely in",count,"seconds at a velocity of",round(velocity,2),"m/s.")
             print("You had",fuel_remaining,"liters of fuel remaining. Game over.")
             #Initialize play function to ask player if they want to play again
             play()
 
         else:
-            #Player loses if velocity is greater than 10 m/s
+            #Player loses if velocity is greater than or equal to 10 m/s
             print(landerfailureart)
-            print("You've crashed and burned after",count,"seconds at a velocity of",velocity,".")
+            print("You've crashed and burned after",count,"seconds at a velocity of",round(velocity,2),"m/s.")
             print("You had", fuel_remaining, "liters of fuel remaining. Game over.")
             # Initialize play function to ask player if they want to play again
             play()

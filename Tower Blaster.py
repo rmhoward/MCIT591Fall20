94f28cee3492d87007178478c31d185bf4e46df8
rmhoward <rmhoward@seas.upenn.edu>
2020-09-27 17:09:13 +0500

    Initial commit
    Signed-off-by: rmhoward <rmhoward@seas.upenn.edu>

--- Tower Blaster.py
+++ Tower Blaster.py
@@ -1,21 +1,20 @@
 import random
 
-
 def print_instructions():
-    """Returns the instructions for the game. Parameters: None.
-       Art taken from https://ascii.co.uk/art/towers and http://patorjk.com/software/taag/#p=display&f=Big&t=Tower%20Blaster"""
-    return ("""
-  _______                       ____  _           _
- |__   __|                     |  _ \| |         | |
-    | | _____      _____ _ __  | |_) | | __ _ ___| |_ ___ _ __
+  """Returns the instructions for the game. Parameters: None.
+     Art taken from https://ascii.co.uk/art/towers and http://patorjk.com/software/taag/#p=display&f=Big&t=Tower%20Blaster"""
+  return ("""
+  _______                       ____  _           _            
+ |__   __|                     |  _ \| |         | |           
+    | | _____      _____ _ __  | |_) | | __ _ ___| |_ ___ _ __ 
     | |/ _ \ \ /\ / / _ \ '__| |  _ <| |/ _` / __| __/ _ \ '__|
-    | | (_) \ V  V /  __/ |    | |_) | | (_| \__ \ ||  __/ |
-    |_|\___/ \_/\_/ \___|_|    |____/|_|\__,_|___/\__\___|_|
-    \n
+    | | (_) \ V  V /  __/ |    | |_) | | (_| \__ \ ||  __/ |   
+    |_|\___/ \_/\_/ \___|_|    |____/|_|\__,_|___/\__\___|_|  
+    \n 
 Instructions: Stabilize your tower by putting the blocks in numerical order with the highest number at the bottom
-and the lowest number on the top.  You will always have two blocks to choose from: The one at the top of the discard
+and the lowest number on the top.  You will always have two blocks to choose from: The one at the top of the discard 
 pile and a mystery block.  Choose wisely! The first player to stabilize their tower wins.
-
+                 
                                 |>>>
                                 |
                             _  _|_  _
@@ -25,20 +24,19 @@ pile and a mystery block.  Choose wisely! The first player to stabilize their to
                              ||:   |
                              ||:.  |
                              ||:  .|
-                             ||:   |
-                             ||: , |
+                             ||:   |       
+                             ||: , |            
                              ||:   |
                              ||: . |
                              ||_   |
   """)
 
-
 def setup_bricks():
     main_pile = []
     discard = []
-    for number in range(1, 61):
+    for number in range(1,61):
         main_pile.append(number)
-    return (main_pile, discard)
+    return (main_pile,discard)
 
 
 def shuffle_bricks(bricks):
@@ -48,28 +46,25 @@ def shuffle_bricks(bricks):
 
 def check_bricks(main_pile, discard):
     if len(main_pile) == 0:
-        shuffle_bricks(discard)
-        for brick in discard:
-            main_pile.append(brick)
-            discard.pop(brick)
-        discard.append(main_pile[0])
-    return (main_pile, discard)
+        discard = shuffle_bricks(discard)
+        main_pile = main_pile.extend(discard)
+        main_pile_first_element = main_pile[0]
+        discard.append(main_pile_first_element)
+        main_pile.pop[0]
+    return main_pile, discard
 
 
 def check_tower_blaster(tower):
-    previous = tower[0]
-    for number in tower:
-        if number < previous:
-            return False
-        previous = number
-    return True
+    if tower != sorted(tower):
+        return False
+    else:
+        return True
 
 
 def get_top_brick(brick_pile):
-    brick = []
-    brick.append(brick_pile[0])
-    brick_pile.remove(brick_pile[0])
-    return brick[0]
+    top_brick = brick_pile[0]
+    del brick_pile[0]
+    return top_brick
 
 
 def deal_initial_bricks(main_pile):
@@ -84,17 +79,18 @@ def deal_initial_bricks(main_pile):
 
 
 def add_brick_to_discard(brick, discard):
-    discard.insert(0, brick)
+    discard.insert(0,brick)
     return discard
-
 
 def find_and_replace(new_brick, brick_to_be_replaced, tower, discard):
     if brick_to_be_replaced in tower:
         brick_position = tower.index(brick_to_be_replaced)
         tower.remove(tower[brick_position])
         tower.insert(brick_position, new_brick)
-        add_brick_to_discard(brick_to_be_replaced, discard)
+        add_brick_to_discard(brick_to_be_replaced,discard)
         return True
+    else:
+        return False
 
 
 def computer_play(tower, main_pile, discard):
@@ -153,41 +149,43 @@ def computer_play(tower, main_pile, discard):
                     break
     return tower, pile_choice
 
+
 def print_tower(tower):
     for i in range(10):
         print("\t\t" + str(tower[i]))
     return ""
 
-
 def choose_pile(main_pile, discard):
     while True:
         pile_choice = input("Type 'D' to take the discard brick, 'M' to take the mystery brick, or 'H' for help.\n")
 
-        if pile_choice[0] == 'D' or pile_choice[0] == 'd':
+        if  pile_choice[0] == 'D' or  pile_choice[0] == 'd':
             discard_pile_brick = get_top_brick(discard)
             which_pile_picked = "discard"
             return discard_pile_brick, which_pile_picked
 
-        if pile_choice[0] == 'M' or pile_choice[0] == 'm':
+        if  pile_choice[0] == 'M' or  pile_choice[0] == 'm':
             main_pile_brick = get_top_brick(main_pile)
             which_pile_picked = "main_pile"
             return main_pile_brick, which_pile_picked
 
-        # if pile_choice[0] == 'H' or pile
+        if pile_choice[0] == 'H' or pile_choice[0] == 'H':
+            print("You can decide to use the known mystery brick")
+
+
 
 
 def ask_yes_or_no(prompt):
-    """Asks the player if they would like to perform an action again. Parameters: prompt"""
-    while True:
-        action_again = input(prompt)
-        # This function only considers the first letter in the string. So, "Ysdfsdf" returns True and "Nsdfsdl" returns False.
-        if action_again[0] == "y" or action_again[0] == "Y":
-            return True
-        elif action_again[0] == "n" or action_again[0] == "N":
-            return False
-        else:
-            print("Please enter Y or N to continue\n")
-
+  """Asks the player if they would like to perform an action again. Parameters: prompt"""
+  while True:
+    action_again = input(prompt)
+    #This function only considers the first letter in the string. So, "Ysdfsdf" returns True and "Nsdfsdl" returns False.
+    if action_again[0] == "y" or action_again[0]== "Y":
+       return True
+    elif action_again[0] == "n" or action_again[0]== "N":
+       return False
+    else:
+       print("Please enter Y or N to continue\n")
 
 def which_brick_to_replace(tower):
     brick_to_replace = None
@@ -209,35 +207,42 @@ def main():
     discard = []
     human_tower = []
     computer_tower = []
+    brick_pile = []
+
     print(print_instructions())
-    main_pile, discard = setup_bricks()
+    main_pile,discard = setup_bricks()
     main_pile = shuffle_bricks(main_pile)
-    # The human and computer towers are created
-    human_tower, computer_tower = deal_initial_bricks(main_pile)
-    # The top brick of the main pile is turned over to begin the discard pile
-    discard.append(get_top_brick(main_pile))
-    # Print the computer tower and your tower at the beginning of the game
+
+    #The human and computer towers are created
+    human_tower,computer_tower = deal_initial_bricks(main_pile)
+
+    #The top brick of the main pile is turned over to begin the discard pile
+    discard.append(int(get_top_brick(main_pile)))
+
+
+    #Print the computer tower and your tower at the beginning of the game in a vertical structure
     print("Computer Tower \t Your Tower")
     for i in range(10):
         print("\t" + str(computer_tower[i]) + "\t\t\t\t" + str(human_tower[i]))
-    print("/ " * 25)
-    while check_tower_blaster(computer_tower) == False and check_tower_blaster(human_tower) == False:
-        # Check to make sure there are bricks in the main pile
-        main_pile, discard = check_bricks(main_pile, discard)
-        print("\nCOMPUTER'S TURN")
-        computer_tower = computer_play(computer_tower, main_pile, discard)
+    print("/ "*25)
+
+    while check_tower_blaster(computer_tower)== False and check_tower_blaster(human_tower) == False:
+        print("COMPUTER'S TURN")
+        computer_tower, pile_choice = computer_play(computer_tower, main_pile,discard)
+        print("The computer replaced a brick with one from the", pile_choice)
         if check_tower_blaster(computer_tower) == True:
             print("The computer has won the game with a tower of:")
             print(print_tower(computer_tower))
             break
 
         print()
-        print("/ " * 25 + "\n")
+        print("/ " * 25 +"\n")
+
         print("YOUR TURN")
-        main_pile, discard = check_bricks(main_pile, discard)
         print("This is your current tower:")
         print(print_tower(human_tower))
-        print("The top card on the discard pile is", discard[0], "\n")
+        main_pile, discard = check_bricks(main_pile, discard)
+        print("The top card on the discard pile is", get_top_brick(discard), "\n")
         user_brick, pile_choice = choose_pile(main_pile, discard)
         print("You picked", user_brick, "from the", pile_choice)
         if pile_choice != "discard":
@@ -264,6 +269,5 @@ def main():
             print(print_tower(human_tower))
 
     print("\nGame Over")
-
 
 main()
